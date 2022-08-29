0aa3df808cf0f355adfbe69bad03d4135bd49356
Tucker Gillbrand <810180@seq.org>
2022-08-29 13:36:27 -0700

    500 Balls now work
    works

--- Lab 8-23/ball-2.js
+++ Lab 8-23/ball-2.js
@@ -1,75 +1,77 @@
 //Part B
-//Wait for the page to finish loading with init(?) as the callback
+//Wait for the page to finish loading with init as the callback
 window.addEventListener("load", init);
 
-//global variables use var bc global
+//global variables
 let canvas, context;
 let balls = [];
-
-function init()
-{
+let vals = [1,2,3,4,5,6,7,8,9,"A","B","C","E","F"];
+console.log(vals[Math.floor(Math.random()*15)]);
+function init() {
     // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
-    canvas = document.getElementById("cnv");//makes it so that this js code links to the canvas in the html
+    canvas = document.getElementById("cnv");//links to first canvas
     // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
-    context = canvas.getContext("2d");//makes the canvas 2d
-    loadBalls(100)
+    context = canvas.getContext("2d");
+    loadBalls(500)
     animate();      //begins the animation
 }
-
-
-function loadBalls(n){
-
-    for(let  i = 0; i < n; i++){
-    let x = Math.random()*canvas.width;
-    let y = Math.random()*canvas.height;
-    let dx = Math.random()*6 - 3;
-    let dy = Math.random()*6 - 3;
-      balls.push(new Ball(x, y, dx, dy, 30))
+function randColor() {
+    let vals = [1,2,3,4,5,6,7,8,9,"A","B","C","E","F"];//Hexcode values
+    let output = "";
+    for(let i = 0; i<6;i++){
+        output += (vals[Math.floor(Math.random()*15)])//gets a random color for the output
+    }
+    return "#" + output;
+}
+function loadBalls(n) {
+    for (let i = 0; i < n; i++) {
+        let x = Math.random() * canvas.width;
+        let y = Math.random() * canvas.height;
+        let dx = Math.random() * 6 - 3;
+        let dy = Math.random() * 6 - 3;
+        let r = Math.random() * 10 + 5;
+        balls.push(new Ball(x, y, dx, dy, r, randColor()))//adds X new ball-protype objects to array
     }
 }
 
-function animate(){
-    //first need to clear last frame of animation
-    context.clearRect(0,0, canvas.width, canvas.height);//this clears all of the stuff
-    bounce();
-    update();
-    render();
+function animate() {
+    context.clearRect(0, 0, canvas.width, canvas.height);//this clears all of the stuff
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].run();
+    }
     requestAnimationFrame(animate);//begins next cycle
 }
 
-function bounce(){
-    if((x+dx)>canvas.width)//checks if x+dx is going to go over the width and swaps it if necessary
-    {
-        dx = -(Math.random()*4)+0.01//math.random to make it more spicy
-            //dont know if the +0.01 is necessary, but dont want to get it stuck on 0 speed 
-    } else if((x+dx)<0)
-    {
-        dx = (Math.random()*4)+0.01
-    }
-    if((y+dy)>canvas.height)
-    {
-        dy = -(Math.random()*4)+0.01
-    } else if((y+dy)<0)
-    {
-        dy = (Math.random()*4)+0.01
-    }
-}
+// function bounce() {
+//     if ((x + dx) > canvas.width)//checks if x+dx is going to go over the width and swaps it if necessary
+//     {
+//         dx = -(Math.random() * 4) + 0.01//math.random to make it more spicy
+//         //dont know if the +0.01 is necessary, but dont want to get it stuck on 0 speed 
+//     } else if ((x + dx) < 0) {
+//         dx = (Math.random() * 4) + 0.01
+//     }
+//     if ((y + dy) > canvas.height) {
+//         dy = -(Math.random() * 4) + 0.01
+//     } else if ((y + dy) < 0) {
+//         dy = (Math.random() * 4) + 0.01
+//     }
+// }
 
-function update(){
-    x += dx;//adds dx/dy to x/y so that the ball moves
-    y += dy;
-}
+// function update() {
+//     x += dx;//adds dx/dy to x/y so that the ball moves
+//     y += dy;
+// }
 
-function render(){
-    
-    let radius = 15; //decides the radius of ball
-    //create circle path
-    context.beginPath();    //clear old path
-    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
-    context.arc(x,y,radius, 0, 2*Math.PI);
-    context.strokeStyle = 0xff0000; // fill color
-    context.fillStyle = 0xff0000; //color to stroke
-    context.fill();     //renders fill
-    context.stroke();   //reners stroke
+// function render() {
 
-}
+//     let radius = 15; //decides the radius of ball
+//     //create circle path
+//     context.beginPath();    //clear old path
+//     // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
+//     context.arc(x, y, radius, 0, 2 * Math.PI);
+//     context.strokeStyle = 0xff0000; // fill color
+//     context.fillStyle = 0xff0000; //color to stroke
+//     context.fill();     //renders fill
+//     context.stroke();   //reners stroke
+
+// }
--- Lab 8-23/ball.js
+++ Lab 8-23/ball.js
@@ -1,54 +1,53 @@
-
-
-function Ball(x, y, dx, dy, r){
-  this.x = x;
-  this.y = y;
-  this.dx = dx;
-  this.dy = dy;
-  this.r = r;
+function Ball(x, y, dx, dy, r, c, vector) {//acts like a class
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.r = r;
+    this.c = c;
+    //this.vector = {
+    //    vx = vx;
+    //    vy = vy;
+    //}
+}
+//function nomenclature: Prototype name.prototype.Function name = function(){}
+Ball.prototype.run = function () {//runs everything locally
+    this.update();
+    this.bounce();
+    this.render();
 }
 
-Ball.prototype.run = function(){
-  this.update();
-  this.bounce();
-  this.render();
-}
 
 
-Ball.prototype.render = function(){
-
-    context.beginPath();    //clear old path
+Ball.prototype.render = function () {
+    context.beginPath();                //clear old path
     // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
-    context.arc(this.x,this.y,this.r, 0, 2*Math.PI);
-    context.strokeStyle = 0xff0000; // fill color
-    context.fillStyle = 0xff0000; //color to stroke
-    context.fill();     //renders fill
-    context.stroke();   //reners stroke
-
+    context.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+    context.strokeStyle = this.c;     // fill color
+    context.fillStyle = this.c;       //color to stroke
+    context.fill();                     //renders fill
+    context.stroke();                   //reners stroke
 }
 
-Ball.prototype.update = function(){
+Ball.prototype.update = function () {
 
     this.x += this.dx;
     this.y += this.dy
 
 }
 
-Ball.prototype.bounce = function(){
-  if((this.x+this.dx)>canvas.width)//checks if x+dx is going to go over the width and swaps it if necessary
-    {
-        this.dx = -(Math.random()*4)+0.01//math.random to make it more spicy
-            //dont know if the +0.01 is necessary, but dont want to get it stuck on 0 speed 
-    } else if((this.x+this.dx)<0)
-    {
-        this.dx = (Math.random()*4)+0.01
+Ball.prototype.bounce = function () {
+    if ((this.x + this.dx) > canvas.width) {//checks if x+dx is going to go over the width and swaps it if necessary
+        this.dx = -(Math.random() * 4) + 1
+        //dont know if the +0.01 is necessary, but dont want to get it stuck on 0 speed 
+    } else if ((this.x + this.dx) < 0) {
+        this.dx = (Math.random() * 4) + 1
     }
-    if((this.y+this.dy)>canvas.height)
-    {
-        this.dy = -(Math.random()*4)+0.01
-    } else if((this.y+this.dy)<0)
-    {
-        this.dy = (Math.random()*4)+0.01
+    if ((this.y + this.dy) > canvas.height) {
+        this.dy = -(Math.random() * 4) + 1
+    } else if ((this.y + this.dy) < 0) {
+        this.dy = (Math.random() * 4) + 1
     }
 }
 
+    
