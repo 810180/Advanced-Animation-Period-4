84f34b4cb0237af87e3c3b78efd3cd096ce48a61
Tucker Gillbrand <810180@seq.org>
2022-09-16 22:34:41 -0700

    9-16 Rocket fixed Asteroids added
    Asteroids added to background. Planet colored. Trail added and fixed and correctly colored.

--- Lab 9-16 RocketShip/main.js
+++ Lab 9-16 RocketShip/main.js
@@ -5,22 +5,35 @@ window.addEventListener("load", init);
 let canvas, context;
 let rocket;
 let planet;
+let trail;
+var asteroid = [];
 
 function init() {
     canvas = document.getElementById("cnv");
     context = canvas.getContext("2d");
     rocket = new Rocket(Math.random()*canvas.width, Math.random()*canvas.height, 40);
     planet = new Rocket(canvas.width-rocket.loc.x,canvas.height-rocket.loc.y, 40);
+    trail = new Rocket(rocket.loc.x,rocket.loc.y, 40);
     planet.vel.x=0;
     planet.vel.y=0;
+    loadAsteroid(10);
     animate();      // kick off the animation
 }
 
-
+function loadAsteroid(n) {
+    for(let i =0;i<n;i++){
+        asteroid.push(new Rocket(Math.random()*canvas.width, Math.random()*canvas.height, 10));
+    }
+}
 
 function animate() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    rocket.run();
+    trail.run();//needs to be ahead of rocket in code
+    rocket.run();//rocket 
+    for(let i = 0; i<asteroid.length;i++){
+        asteroid[i].run();
+    }
+    
     planet.run();
     requestAnimationFrame(animate); // next cycle
 }
--- Lab 9-16 RocketShip/rocket.js
+++ Lab 9-16 RocketShip/rocket.js
@@ -5,42 +5,68 @@ function Rocket(x, y, d) {
   this.vel = new JSVector(dx, dy); // add a velocity vector
   this.acc = new JSVector(0, 0);//  add an acceleration vector
   this.diam = d;
-  this.clr = "#FF0000";
+  this.clr;
 }
 
 Rocket.prototype.run = function () {
   this.render();
+  trail.vel.x=rocket.vel.x;
+  trail.vel.y=rocket.vel.y;
+  trail.acc.x=rocket.acc.x;
+  trail.acc.y=rocket.acc.y;
   this.update();
   this.edgeCheck();
 }
 
 Rocket.prototype.render = function () {
+  for(let i = 0;i<asteroid.length;i++){
+    if(this===asteroid[i]){//asteroid rendering
+      this.clr = "#808080";
+      context.beginPath();
+      context.arc(this.loc.x, this.loc.y, this.diam, 0, 2 * Math.PI);
+      context.strokeStyle = this.clr;
+      context.fillStyle = this.clr;
+      context.fill();
+      context.stroke();
+    }
+  }
   if(this===rocket){
+    this.clr = "#FFC0CB";
   context.beginPath();    // clear old path
   context.save();
     context.translate(this.loc.x, this.loc.y);
     context.rotate(this.vel.getDirection());
     //rocket drawing here
-    context.strokeStyle = "#FFC0CB";
+    context.strokeStyle = this.clr;
     context.moveTo(this.diam,0);
     context.lineTo(0,this.diam/4);
     context.lineTo(-this.diam/8,0);
     context.lineTo(0,-this.diam/4);
     context.closePath();
-    context.fillStyle = "#FFC0CB"
+    context.fillStyle = this.clr;
     context.fill();
+   context.restore();
+   context.stroke();   // render the stroke
+  }
+  if(this===trail){
+    this.clr = "red";//causes an asteroid to turn red
+    context.save();
+    context.translate(this.loc.x, this.loc.y);
+    context.rotate(this.vel.getDirection());
+    context.strokeStyle = this.clr;
     //flame drawing here
     context.moveTo(-this.diam/8,0);
     context.lineTo(-this.diam/8,this.diam/8);
     context.lineTo(-this.diam/4*Math.abs(rocket.vel.getMagnitude()),0);
-    context.lineTo(-this.diam/8,this.diam/8);
+    context.lineTo(-this.diam/8,-this.diam/8);
     context.closePath();
-    //context.fillStyle= "#FF0000"
-    //context.fill();
-   context.restore();
-   context.stroke();   // render the stroke
-   
-  } else if(this===planet){
+    context.fillStyle = this.clr;
+    context.fill();
+    context.restore();
+    context.stroke();
+  }
+  if(this===planet){
+    this.clr = "red";
     context.beginPath();
     context.arc(this.loc.x, this.loc.y, this.diam, 0, 2 * Math.PI);  //  change x and y to this.loc.x and this.loc.y
     context.strokeStyle = "black";  // color to fill
@@ -51,14 +77,14 @@ Rocket.prototype.render = function () {
 }
 
 Rocket.prototype.update = function () {
-  if(this === rocket){
+  if(this === rocket||this=== trail){
     
     let d = this.loc.distance(planet.loc);
     this.acc = JSVector.subGetNew(planet.loc,this.loc);
     this.acc.normalize();
     this.acc.multiply(0.05);
     this.vel.add(this.acc);
-    this.vel.limit(3);
+    this.vel.limit(3.05);//rocket must be slighly faster in order to catch up to planet
     
   } else if(this === planet && this.loc.distance(rocket.loc)<150){
     let d = this.loc.distance(rocket.loc);
@@ -67,7 +93,7 @@ Rocket.prototype.update = function () {
     this.acc.multiply(0.05);
     this.vel.add(this.acc);
     this.vel.limit(3);
-    if(this.loc.distance(rocket.loc)<100){
+    if(this.loc.distance(rocket.loc)<this.diam*2){//rocket has to touch 
       this.loc.x = Math.random()*canvas.width;
       this.loc.y = Math.random()*canvas.height;
     }
