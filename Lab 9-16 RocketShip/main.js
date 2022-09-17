628539eb59f8bece65ce69be521ca88a1190977c
Tucker Gillbrand <810180@seq.org>
2022-09-16 23:55:16 -0700

    9-16 Comments
    Canvas now covers all of the window. Comments added to make code slightly more readable.

--- Lab 9-16 RocketShip/main.js
+++ Lab 9-16 RocketShip/main.js
@@ -7,33 +7,40 @@ let rocket;
 let planet;
 let trail;
 var asteroid = [];
+//asteroid is a global to let each asteroid run invidually
 
 function init() {
     canvas = document.getElementById("cnv");
     context = canvas.getContext("2d");
-    rocket = new Rocket(Math.random()*canvas.width, Math.random()*canvas.height, 40);
-    planet = new Rocket(canvas.width-rocket.loc.x,canvas.height-rocket.loc.y, 40);
-    trail = new Rocket(rocket.loc.x,rocket.loc.y, 40);
-    planet.vel.x=0;
-    planet.vel.y=0;
+    //width and height are set here instead of in indext to make it full page window
+    context.canvas.width = window.innerWidth;
+    context.canvas.height = window.innerHeight;
+    //rocket,planet, and trail are initialized here
+    rocket = new Rocket(Math.random() * canvas.width, Math.random() * canvas.height, 40);
+    planet = new Rocket(canvas.width - rocket.loc.x, canvas.height - rocket.loc.y, 40);
+    trail = new Rocket(rocket.loc.x, rocket.loc.y, 40);//trail location is matched with rocket's
+    //planet velocity is set to 0 manually to prevent the planet from moving around before the rocket gets close
+    planet.vel.x = 0;
+    planet.vel.y = 0;
     loadAsteroid(10);
     animate();      // kick off the animation
 }
 
 function loadAsteroid(n) {
-    for(let i =0;i<n;i++){
-        asteroid.push(new Rocket(Math.random()*canvas.width, Math.random()*canvas.height, 10));
+    //load asteroid is its own function so it can scale
+    for (let i = 0; i < n; i++) {
+        asteroid.push(new Rocket(Math.random() * canvas.width, Math.random() * canvas.height, 10));
     }
 }
 
 function animate() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     trail.run();//needs to be ahead of rocket in code
-    rocket.run();//rocket 
-    for(let i = 0; i<asteroid.length;i++){
+    //running the trail AFTER the asteroid changes the asteroid with the last indext to be the color of the trail
+    rocket.run();//rocket is currently below asteroids when rendering
+    for (let i = 0; i < asteroid.length; i++) {
         asteroid[i].run();
     }
-    
     planet.run();
     requestAnimationFrame(animate); // next cycle
 }
--- Lab 9-16 RocketShip/rocket.js
+++ Lab 9-16 RocketShip/rocket.js
@@ -10,18 +10,20 @@ function Rocket(x, y, d) {
 
 Rocket.prototype.run = function () {
   this.render();
-  trail.vel.x=rocket.vel.x;
-  trail.vel.y=rocket.vel.y;
-  trail.acc.x=rocket.acc.x;
-  trail.acc.y=rocket.acc.y;
+  //trail velocity and accel have to be manually matched here(could be set in update?)
+  trail.vel.x = rocket.vel.x;
+  trail.vel.y = rocket.vel.y;
+  trail.acc.x = rocket.acc.x;
+  trail.acc.y = rocket.acc.y;
   this.update();
   this.edgeCheck();
 }
 
 Rocket.prototype.render = function () {
-  for(let i = 0;i<asteroid.length;i++){
-    if(this===asteroid[i]){//asteroid rendering
-      this.clr = "#808080";
+  for (let i = 0; i < asteroid.length; i++) {
+    if (this === asteroid[i]) {
+      //asteroid rendering
+      this.clr = "#808080";//this.clr is set here instead of in initialzing portion to make it keep colors for objects seperate
       context.beginPath();
       context.arc(this.loc.x, this.loc.y, this.diam, 0, 2 * Math.PI);
       context.strokeStyle = this.clr;
@@ -30,90 +32,93 @@ Rocket.prototype.render = function () {
       context.stroke();
     }
   }
-  if(this===rocket){
+  if (this === rocket) {
     this.clr = "#FFC0CB";
-  context.beginPath();    // clear old path
-  context.save();
+    context.beginPath();    // clear old path
+    context.save();
     context.translate(this.loc.x, this.loc.y);
     context.rotate(this.vel.getDirection());
     //rocket drawing here
     context.strokeStyle = this.clr;
-    context.moveTo(this.diam,0);
-    context.lineTo(0,this.diam/4);
-    context.lineTo(-this.diam/8,0);
-    context.lineTo(0,-this.diam/4);
+    context.moveTo(this.diam, 0);
+    context.lineTo(0, this.diam / 4);
+    context.lineTo(-this.diam / 8, 0);
+    context.lineTo(0, -this.diam / 4);
     context.closePath();
     context.fillStyle = this.clr;
     context.fill();
-   context.restore();
-   context.stroke();   // render the stroke
+    context.restore();
+    context.stroke();   // render the stroke
   }
-  if(this===trail){
-    this.clr = "red";//causes an asteroid to turn red
+  if (this === trail) {//the trail and rocket have to be seperate objects
+    this.clr = "red";
     context.save();
     context.translate(this.loc.x, this.loc.y);
     context.rotate(this.vel.getDirection());
     context.strokeStyle = this.clr;
     //flame drawing here
-    context.moveTo(-this.diam/8,0);
-    context.lineTo(-this.diam/8,this.diam/8);
-    context.lineTo(-this.diam/4*Math.abs(rocket.vel.getMagnitude()),0);
-    context.lineTo(-this.diam/8,-this.diam/8);
-    context.closePath();
+    context.moveTo(-this.diam / 8, 0);
+    context.lineTo(-this.diam / 8, this.diam / 8);
+    context.lineTo(-this.diam / 4 * Math.abs(rocket.vel.getMagnitude()), 0);
+    context.lineTo(-this.diam / 8, -this.diam / 8);
+    context.closePath();//close path will help fill the object
     context.fillStyle = this.clr;
     context.fill();
     context.restore();
     context.stroke();
   }
-  if(this===planet){
+  if (this === planet) {
     this.clr = "red";
     context.beginPath();
-    context.arc(this.loc.x, this.loc.y, this.diam, 0, 2 * Math.PI);  //  change x and y to this.loc.x and this.loc.y
-    context.strokeStyle = "black";  // color to fill
-    context.fillStyle = this.clr;     // color to stroke
-    context.fill();     // render the fill
-    context.stroke();   // render the stroke
+    context.arc(this.loc.x, this.loc.y, this.diam, 0, 2 * Math.PI);//standard circle generation
+    context.strokeStyle = "black";
+    context.fillStyle = this.clr;
+    context.fill();
+    context.stroke();
   }
 }
 
 Rocket.prototype.update = function () {
-  if(this === rocket||this=== trail){
-    
+  if (this === rocket || this === trail) {
+
     let d = this.loc.distance(planet.loc);
-    this.acc = JSVector.subGetNew(planet.loc,this.loc);
+    this.acc = JSVector.subGetNew(planet.loc, this.loc);
     this.acc.normalize();
     this.acc.multiply(0.05);
     this.vel.add(this.acc);
     this.vel.limit(3.05);//rocket must be slighly faster in order to catch up to planet
-    
-  } else if(this === planet && this.loc.distance(rocket.loc)<150){
+
+  } else if (this === planet && this.loc.distance(rocket.loc) < 150) {
     let d = this.loc.distance(rocket.loc);
-    this.acc = JSVector.subGetNew(this.loc,rocket.loc);
+    this.acc = JSVector.subGetNew(this.loc, rocket.loc);
+    //acceleration is calculated and limited here
     this.acc.normalize();
     this.acc.multiply(0.05);
     this.vel.add(this.acc);
     this.vel.limit(3);
-    if(this.loc.distance(rocket.loc)<this.diam*2){//rocket has to touch 
-      this.loc.x = Math.random()*canvas.width;
-      this.loc.y = Math.random()*canvas.height;
+    if (this.loc.distance(rocket.loc) < this.diam * 2) {//checks if the rocket touches the planet to moves planet
+      this.loc.x = Math.random() * canvas.width;
+      this.loc.y = Math.random() * canvas.height;
     }
-  } else if(this === planet && this.loc.distance(rocket.loc)>200 && this.vel.getMagnitude()>0){
-    this.vel.setMagnitude(this.vel.getMagnitude()-0.05);
+  } else if (this === planet && this.loc.distance(rocket.loc) > 200 && this.vel.getMagnitude() > 0) {
+    //if the planet is far from the rocket then it stops acceleration
+    this.vel.setMagnitude(this.vel.getMagnitude() - 0.05);
   }
   this.loc.add(this.vel);
 }
 
 Rocket.prototype.edgeCheck = function () {
+  //rocket/trail/planet/asteroids will loop back to other side once they reach the edge
   if (this.loc.y > canvas.height) {
     this.loc.y = 0;
   }
-  if(this.loc.y<0){
+  if (this.loc.y < 0) {
     this.loc.y = canvas.height;
   }
   if (this.loc.x > canvas.width) {
     this.loc.x = 0;
   }
-  if(this.loc.x<0){
+  if (this.loc.x < 0) {
     this.loc.x = canvas.width;
   }
 }
