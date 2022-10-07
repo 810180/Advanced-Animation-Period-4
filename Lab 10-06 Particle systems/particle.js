63da6192191fa67685aa67237db16228fff1169d
Tucker Gillbrand <810180@seq.org>
2022-10-07 12:23:10 -0700

    10-07 Particle system fixed
    Particle system fixed, gives people epilepsy on purpose.

--- Lab 10-06 Particle systems/particle.js
+++ Lab 10-06 Particle systems/particle.js
@@ -3,27 +3,30 @@ function Particle (x,y,death,ctx1,ctx2,rad) {
     this.vel = new JSVector(Math.random()*4-2,Math.random()*4-2);
     this.acc = new JSVector(0,0.05);
     this.dDayDay = death;
-    this.lmao = false;
+    this.lmao = false;//is the particle dead?
+    //swapped because it seems funny
     this.ctx1 = ctx2;//This is the Mini Context
     this.ctx2 = ctx1;//This is the MAIN context
     this.rad = rad;
+    //this.clr = clr I want to give ppl epileptic fits
+}
+Particle.prototype.run = function () {
+    this.update();
+    this.render();
+    this.die();
 }
 Particle.prototype.die = function () {
     this.dDayDay -= 1;
     if(this.dDayDay <= 0){
-        this.lmao = true;       
+        this.lmao = true;
      }
-}
-Particle.prototype.run = function () {
-    this.die;
-    this.update;
-    this.render;
 }
 Particle.prototype.update = function () {
     this.vel.add(this.acc);
     this.loc.add(this.vel)
 }
 Particle.prototype.render = function () {
+    //Main Context
     this.ctx2.beginPath();
     this.ctx2.arc(this.loc.x,this.loc.y,this.rad,0,Math.PI*2);
     this.ctx2.closePath();
@@ -31,4 +34,12 @@ Particle.prototype.render = function () {
     this.ctx2.strokeStyle = world.getRandomColor();
     this.ctx2.fill();
     this.ctx2.stroke();
+    //Mini context
+    this.ctx1.beginPath();
+    this.ctx1.arc(this.loc.x,this.loc.y,this.rad,0,Math.PI*2);
+    this.ctx1.closePath();
+    this.ctx1.fillStyle = world.getRandomColor();
+    this.ctx1.strokeStyle = world.getRandomColor();
+    this.ctx1.fill();
+    this.ctx1.stroke();
 }
--- Lab 10-06 Particle systems/particlesystems.js
+++ Lab 10-06 Particle systems/particlesystems.js
@@ -1,24 +1,44 @@
 function particleSystems(x,y,ctx1,ctx2){
     this.sLoc = new JSVector(x,y);
+    this.sVel = new JSVector(Math.random()*4-2,Math.random()*4-2)
     this.particles = [];
     this.ctx1 = ctx1;
     this.ctx2 = ctx2;
     //random death day
 }
 particleSystems.prototype.newparticle = function () {
-    this.particles[this.particles.length] = (new Particle(this.sLoc.x,this.sLoc.y, Math.random()*500 + 500,this.ctx1,this.ctx2));
+    this.particles[this.particles.length] = (new Particle(this.sLoc.x,this.sLoc.y, Math.random()*100 + 100,this.ctx1,this.ctx2, 2));
 }
 particleSystems.prototype.lmaoF = function () {
-    for(let i = this.particles.length-1;i>=0;i++){
-        if(this.particles[i].lamo == true){
+    for(let i = this.particles.length-1;i>=0;i--){
+        if(this.particles[i].lmao == true){
             this.particles.splice(i,1);
         }
     }
 }
 particleSystems.prototype.run = function () {
-    this.newparticle();
-    this.lmaoF();
+    this.newparticle();//creates a new particle every phrame
     for(let i = 0; i<this.particles.length; i++){
-        this.particles[i].run()
+        this.particles[i].run();
     }
+    this.lmaoF();//checks to make sure the particle shouldnt die
+    this.move();
+    this.checkEdges();
+}
+particleSystems.prototype.move = function () {
+    this.sLoc.add(this.sVel);
+}
+particleSystems.prototype.checkEdges = function () {
+    if(this.sLoc.x<world.dims.left){
+        this.sVel.x = -this.sVel.x;
+      }
+      if(this.sLoc.x>world.dims.right){
+        this.sVel.x = -this.sVel.x;
+      }
+      if(this.sLoc.y>world.dims.top){
+        this.sVel.y = -this.sVel.y;
+      }
+      if(this.sLoc.y<world.dims.bottom){
+        this.sVel.y = -this.sVel.y;
+      }
 }
