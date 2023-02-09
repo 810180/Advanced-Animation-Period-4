9761e5e2b65bc678b54fb40123d14d2a9e117ae5
Tucker Gillbrand <810180@seq.org>
2023-02-09 12:51:18 -0800

    cATS

--- Lab 2-7 canvas DOM/main.js
+++ Lab 2-7 canvas DOM/main.js
@@ -1,7 +1,9 @@
 
 
 // global variables for canvas and context
-var game, canvas, ctx;
+var game, canvas, ctx,textTest,wrldText;
+var goal = "Hello World";
+var ltrs = ["a","b","c","d","e","f","g","h","i","j","k","i","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
 window.onload = init;//  After the window has been loaded, go to init
 
 function init(){
@@ -12,14 +14,18 @@ function init(){
   canvas.height = 696; // 700 - 4 for the border
   ctx = canvas.getContext('2d'); // This is the context
   game = new Game();
+  wrldText = "Hello World";
+  textTest = document.getElementById("testDiv");
+  this.text = document.createElement("div");
+  this.writing = document.createTextNode(wrldText);
+  textTest.appendChild(this.writing);
   animate();
 }
-
 
 function animate(){
   ctx.fillStyle = 'rgba(0,0,0,.05)'
   ctx.fillRect(0,0,canvas.width,canvas.height); 
-  game.update()
+  game.update();
   requestAnimationFrame(animate);
 }
 
