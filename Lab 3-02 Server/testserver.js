16bf9e827850523cf8fd37093217b8ccf042ea94
Tucker Gillbrand <810180@seq.org>
2023-03-07 10:46:23 -0800

    json package uploaded

--- Lab 3-02 Server/testserver.js
+++ Lab 3-02 Server/testserver.js
@@ -1,16 +1,23 @@
 const http = require('node:http');
 
+const mime = require("mime");
+mime.getType("txt");
+mime.getExtension("text/plain");
+
 // Create a local server to receive data from
 const server = http.createServer();
 
 // Listen to the request event
 server.on('request', (request, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    data: 'Hello World!',
-  }));
+    // console.log(request.method);
+    // console.log(request.url);
+    // console.log(request.headers)
+    //request.
+    let mimeType = mime.getType("html");
+    res.writeHead(200, { 'Content-Type': mimeType });
+    res.end("<html><head><title>Test nodeServer</title><h1>Writing some text to see if it works</h1></head></html>")
 });
 
-server.listen(8000);
+server.listen(8000);//tells the server which port to listen on
 
 console.log("server started on port 8000, press ctrl+c to terminate");
