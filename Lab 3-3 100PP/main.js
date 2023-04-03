ac06574abcaa7a8347491dbb794bc1280cc3de9f
Tucker Gillbrand <810180@seq.org>
2023-04-03 14:03:09 -0700

    d

--- Lab 3-3 100PP/main.js
+++ Lab 3-3 100PP/main.js
@@ -3,29 +3,16 @@ window.addEventListener("load", init);
 
 // global variables
 let canvas, context;
+let prisoners = [];
 function init() {
     canvas = document.getElementById("cnv");
     context = canvas.getContext("2d");
-    animate();      // kick off the animation
 }
+function loadPrisoners(){
 
-function loadMover(n){//
-    for(let i = 0; i < n; i++){
-        //  fill an array with n balls
-        let x = Math.random() * canvas.width;
-        let y = Math.random() * canvas.height;
-        movers.push(new Mover(x,y, 7));
-    }
 }
+function returnAnswer(){
 
-function animate() {
-    //context.clearRect(0, 0, canvas.width, canvas.height);
-    for(let i = 0; i<movers.length;i++){
-        movers[i].run();
-    }
-    attractor.run();
-    //repeller.run();
-    requestAnimationFrame(animate); // next cycle
 }
 
 
