bc7fe2fedfab0fd4af5bb7283b708a7f4313bdf4
Tucker Gillbrand <810180@seq.org>
2023-04-03 14:07:00 -0700

    G

--- Lab 3-3 100PP/main.js
+++ Lab 3-3 100PP/main.js
@@ -4,12 +4,22 @@ window.addEventListener("load", init);
 // global variables
 let canvas, context;
 let prisoners = [];
+let choices = [];
 function init() {
     canvas = document.getElementById("cnv");
     context = canvas.getContext("2d");
+    loadPrisoners(100);
 }
-function loadPrisoners(){
-
+function loadPrisoners(n){
+    for(let i = 0; i<n;i++){
+        prisoners[i] = i;
+    }
+    let tempArray = prisoners;
+    for(let i=0; i<n;i++){
+        let id = Math.floor(Math.random()*100);
+        choices[i]=tempArray[id];
+    }
+    console.log(choices);
 }
 function returnAnswer(){
 
